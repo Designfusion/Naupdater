c99205b6f97d044210ef09adfb00320276f6bbce
Jonathan Brown <jbrown@designfusion.com>
2019-09-26 20:50:41 -0400

    translate

--- Naupdater/Program.cs
+++ Naupdater/Program.cs
@@ -12,27 +12,27 @@ namespace Naupdater
 {
     static class Program
     {
-        public static AppArguments Args; // 运行参数
+        public static AppArguments Args; // Operating parameters
 
         /// <summary>
-        /// 应用程序的主入口点。
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] programArgs)
         {
-            // 检查系统版本
+            // Check system version
             if (!Utils.IsWinVistaOrHigher())
             {
-                MessageBox.Show("不支持的操作系统版本，最低需求为 Windows Vista",
-                "Naupdater 无法运行", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Unsupported operating system version, the minimum requirement is Windows Vista",
+                "Naupdater Can not run", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            // 检测 .NET Framework 版本
+            // Detection .NET Framework version
             if (!Utils.IsSupportedRuntimeVersion())
             {
-                MessageBox.Show("当前 .NET Framework 版本过低，请升级至 4.6.2 或更新版本",
-                "Naupdater 无法运行", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("The current .NET Framework version is too low, please upgrade to 4.6.2 or newer",
+                "Naupdater Can not run", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
                 Process.Start(
                     "http://dotnetsocial.cloudapp.net/GetDotnet?tfm=.NETFramework,Version=v4.6.2");
@@ -56,27 +56,27 @@ namespace Naupdater
                     {
                         Process oldProcess = oldProcesses[0];
                     }
-                    MessageBox.Show($"Naupdater 正在运行中... {Environment.NewLine}无需重复打开");
+                    MessageBox.Show($"Naupdater is running... {Environment.NewLine}No need to open repeatedly");
                     return;
                 }
 
                 Directory.SetCurrentDirectory(Application.StartupPath);
 
-                // 启动参数
+                // Startup parameter
                 ArgsHandle(programArgs);
 
-                // 启动主界面
+                // Start the main interface
                 Application.Run(new UpdaterForm());
             }
         }
 
         /// <summary>
-        /// 启动参数处理
+        /// Start parameter processing
         /// </summary>
         /// <param name="programArgs"></param>
         private static void ArgsHandle(string[] programArgs)
         {
-            // 程序启动参数
+            // Program startup parameter
             var p = new FluentCommandLineParser<AppArguments>();
 
             Program.Args = p.Object;
@@ -84,60 +84,60 @@ namespace Naupdater
             p.Setup(args => args.TargetAppName)
                 .As("name")
                 .SetDefault(Utils.GetAppConfig("TargetAppName", ""))
-                .WithDescription("目标程序名（用于显示）");
+                .WithDescription("Target program name (for display)");
 
             p.Setup(args => args.TargetAppProcessName)
                 .As("process")
                 .SetDefault(Utils.GetAppConfig("TargetAppProcessName", ""))
-                .WithDescription("目标程序进程名（用于强制关闭应用）");
+                .WithDescription("Target program process name (used to force the application to close)");
 
             p.Setup(args => args.TargetAppRootPath)
                 .As("root-path")
                 .SetDefault(Utils.GetAppConfig("TargetAppRootPath", "./"))
-                .WithDescription("目标程序根目录（文件操作将在该目录中进行）");
+                .WithDescription("Target program root directory (file operations will be performed in this directory)");
 
             p.Setup(args => args.SrcDownloadUrl)
                 .As('u', "url")
-                .WithDescription("远程下载升级资源文件地址（在线升级）");
+                .WithDescription("Remote download upgrade resource file address (online upgrade)");
 
             p.Setup(args => args.SrcDownloadProxy)
                 .As('p', "proxy")
                 .SetDefault(Utils.GetAppConfig("DownloadProxy", ""))
-                .WithDescription("远程下载代理");
+                .WithDescription("Remote download agent");
 
             p.Setup(args => args.SrcFileMD5)
                 .As('v', "md5")
-                .WithDescription("下载升级资源文件 MD5 值");
+                .WithDescription("Download the upgrade resource file MD5 value");
 
             p.Setup(args => args.SrcLocalFileName)
                 .As("local-src")
                 .SetDefault("")
-                .WithDescription("本地升级资源文件（离线升级）");
+                .WithDescription("Local upgrade resource file (offline upgrade)");
 
             p.Setup(args => args.LauchAppFileName)
                 .As("lauch")
                 .SetDefault(Utils.GetAppConfig("LauchAppFileName", ""))
-                .WithDescription("升级完成后启动的文件");
+                .WithDescription("Files started after the upgrade is completed");
 
             p.Setup(args => args.LauchAppFileArgs)
                 .As("lauch-args")
                 .SetDefault(Utils.GetAppConfig("LauchAppFileArgs", ""))
-                .WithDescription("升级完成后启动的文件");
+                .WithDescription("Files started after the upgrade is completed");
             
             p.Setup(args => args.UpdateMode)
                 .As('m', "mode")
                 .SetDefault(UpdateMode.OverwriteFiles)
-                .WithDescription("升级模式");
+                .WithDescription("Upgrade mode");
 
             p.SetupHelp("?", "help").Callback((text) => {
-                MessageBox.Show(text, "Naupdater 启动参数", MessageBoxButtons.OK);
+                MessageBox.Show(text, "Naupdater Startup parameter", MessageBoxButtons.OK);
                 ExitApp();
             });
             
             if (p.Parse(programArgs).HasErrors)
-                ReportErrorAndExit("启动参数有误");
+                ReportErrorAndExit("Incorrect startup parameters");
 
-            LauchErrorText = Utils.GetAppConfig("LauchErrorText", "程序无法启动，请尝试重装");
+            LauchErrorText = Utils.GetAppConfig("LauchErrorText", "The program can't start, please try to reload");
 
             // No Args
             if (string.IsNullOrWhiteSpace(Args.SrcDownloadUrl) && string.IsNullOrWhiteSpace(Args.SrcLocalFileName))
@@ -148,7 +148,7 @@ namespace Naupdater
 
                 string msg = Utils.GetAppConfig("NoArgsMsgText", "");
                 if (!String.IsNullOrWhiteSpace(msg))
-                    MessageBox.Show(Utils.GetAppConfig("NoArgsMsgText", "请指定启动参数"), $"{Args.TargetAppName} 升级程序", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show(Utils.GetAppConfig("NoArgsMsgText", "Please specify the startup parameters"), $"{Args.TargetAppName} Upgrade procedure", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 ExitApp();
             }
 
@@ -156,12 +156,12 @@ namespace Naupdater
             Args.TargetAppRootPath = Utils.GetPathBasedOn(Application.StartupPath, Args.TargetAppRootPath);
         }
 
-        public static string LauchErrorText; // 启动程序错误文字
+        public static string LauchErrorText; // Launcher error text
 
         /// <summary>
-        /// 启动程序
+        /// starting program
         /// </summary>
-        /// <param name="AppFilePath">目标程序文件路径</param>
+        /// <param name="AppFilePath">Target program file path</param>
         public static void LauchProgram(string fileName, string args = null)
         {
             try
@@ -175,7 +175,7 @@ namespace Naupdater
         }
 
         /// <summary>
-        /// 错误处理
+        /// Error handling
         /// </summary>
         private static int exited = 0;
 
@@ -195,7 +195,7 @@ namespace Naupdater
             {
                 string appName = !String.IsNullOrWhiteSpace(Args.TargetAppName) ? Args.TargetAppName + " " : "";
 
-                MessageBox.Show($"{error}", $"{appName} 升级失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"{error}", $"{appName} Upgrade failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
                 ExitApp();
             }
